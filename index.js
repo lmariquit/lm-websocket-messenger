fbfc88ea5f38a4eb7bf0f4bfc342ae0e64975552
Loren Mariquit <lmariquit@dotdash.com>
2019-07-11 09:13:27 -0400

    created html

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ var server = app.listen(3000, function () {
 	console.log("You hear that????")
 	console.log("The app is listening on port 3000")
 })
+
+// Static files
+app.use(express.static('public'))
