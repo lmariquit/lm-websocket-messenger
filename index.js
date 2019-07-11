206d3e10e2ef1ed266443d903b08e2bf4be2f923
Loren Mariquit <lmariquit@dotdash.com>
2019-07-11 09:29:08 -0400

    made socket connection between server and client

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express');
+var socket = require('socket.io');
 
 // App setup
 var app = express();
 var server = app.listen(3000, function () {
 	console.log("You hear that????")
 	console.log("The app is listening on port 3000")
-})
+});
 
 // Static files
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket) {
+	console.log(`--> Server has made socket connection!! <--
+		${socket.id}
+
+		`)
+});
---
+++ public/chat.js
@@ -0,0 +1,2 @@
+// Make connection
+var clientSocket = io.connect('http://localhost:3000');
